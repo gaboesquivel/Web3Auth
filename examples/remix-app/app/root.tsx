f0f57eb4b23509f98241ddf39b005e88b1a72798
Gabo Esquivel <contact@gaboesquivel.com>
2022-07-13 17:13:54 -0600

    wip: import external script

--- examples/remix-app/app/root.tsx
+++ examples/remix-app/app/root.tsx
@@ -7,12 +7,29 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { ExternalScripts, ExternalScriptsFunction } from "remix-utils";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+const scripts: ExternalScriptsFunction = () => {
+  return [
+    {
+      src: "https://cdn.jsdelivr.net/npm/@web3auth/web3auth@1.1.1",
+      integrity: "sha384-FBByM2J4GLF0BU0zYGFqksU97eJV2mbpgZhajABFCELVsv+JhGpmGoiK6sAVY8OR",
+      crossOrigin: "anonymous",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/npm/@web3auth/openlogin-adapter@1.1.1",
+      integrity: "sha384-0LuO6v4sRENxqtVz/MmCM2N9eS9b6tRBSwO1+LviZds6PJlC/FbUbHPwljyKLedS", 
+      crossOrigin: "anonymous",
+    },
+  ];
+};
+export const handle = { scripts }
 
 export default function App() {
   return (
@@ -24,6 +41,7 @@ export default function App() {
       <body>
         <Outlet />
         <ScrollRestoration />
+        <ExternalScripts />
         <Scripts />
         <LiveReload />
       </body>
--- examples/remix-app/app/routes/index.tsx
+++ examples/remix-app/app/routes/index.tsx
@@ -1,8 +1,34 @@
 import { useEffect } from "react";
 
+const clientId =
+  'BGg06C3u5cKtQ8pY3sANCcwZe30Ch8qz7xbbd-1RAQsUBPeZThjuG6EH6qeTaBB-VKdii-oeOvp2uemQcHBNxKY'
+
 const useWeb3Auth = () => {
   const initWeb3Auth = async () => {
-    const { Web3Auth } = await import('@web3auth/web3auth')
+    console.log('🔑 initializing web3auth ...')
+
+    if (!window.Web3auth) return console.error('window.Web3auth namespace not found')
+    if (!window.OpenloginAdapter) return console.error('window.OpenloginAdapter namespace not found')
+  
+    const web3auth = new window.Web3auth.Web3Auth({
+      clientId,
+      chainConfig: { chainNamespace: 'eip155', chainId: '0x3' },
+      authMode: 'DAPP',
+    })
+
+    const openloginAdapter = new window.OpenloginAdapter.OpenloginAdapter({
+      adapterSettings: {
+        network: 'testnet',
+        clientId,
+        uxMode: 'popup',
+      },
+    })
+
+    web3auth.configureAdapter(openloginAdapter)
+
+    web3auth.initModal()
+
+    console.log('🔑 web3auth initialized!')
   }
 
   const login = () => {}
